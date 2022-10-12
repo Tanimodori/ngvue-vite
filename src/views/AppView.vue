cc54df16efa7b1c1d895ca80a667dd1bee6d1d09
Tanimodori <unknowner2014@gmail.com>
2022-10-12 14:38:57 +0800

    feat: reactivity test

--- src/views/AppView.vue
+++ src/views/AppView.vue
@@ -1,3 +1,17 @@
 <template>
-  <span>Hello Vue!</span>
+  <div>
+    <div>Hello Vue!</div>
+    <div>Count: {{ count }}</div>
+    <div>Double: {{ double }}</div>
+    <button @click="increment">Increment</button>
+  </div>
 </template>
+<script lang="ts" setup>
+  import { computed, ref } from 'vue';
+
+  const count = ref(0);
+  const double = computed(() => count.value * 2);
+  const increment = () => {
+    count.value++;
+  };
+</script>
