7bb43fa2ec74806e54f1ce809ad5f9a0153b46a3
Tanimodori <unknowner2014@gmail.com>
2022-10-13 10:00:44 +0800

    refactor: using pure vue

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,7 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
+import Vue from 'vue';
+import AppView from './views/AppView.vue';
 
-export async function init() {
-  /**
-   * Load jQuery.
-   * Prohibit AngularJS using jqlite.
-   */
-  const jQuery = await import('jquery');
-  // @ts-ignore
-  globalThis.jQuery = globalThis.$ = globalThis.jqLite = jQuery.default;
-
-  /**
-   * Load AngularJS.
-   */
-  const angular = await import('angular');
-  globalThis.angular = angular.default;
-
-  /**
-   * Load Vue.
-   * Inject Vueuses here if you need.
-   * @example
-   * const Vue = await import('vue');
-   * Vue.default.use(...);
-   */
-  await import('vue');
-  // @ts-ignore
-  await import('ngVue');
-
-  /**
-   * Bootstrap AngularJS.
-   */
-  const app = await import('./app');
-  $(document).ready(function () {
-    angular.bootstrap(document, [app.default]);
-  });
-}
-
-init();
+new Vue({
+  el: '#app',
+  render: (h) => h(AppView),
+});
